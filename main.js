01358b6e1979cd45f082c19096779af2a309634d
chuan6 <chuan6.dev@gmail.com>
2016-07-15 12:14:49 +0800

    introduce dynamic multi-column positioning (window.onresize)
    ...up to 3 columns.

--- main.js
+++ main.js
@@ -16,31 +16,71 @@ function verticalSpan(elmt) {
         + parseFloat(computed.marginBottom);
 }
 
-function position_in_two_cols(es) {
-    var i, e;
-    var leftTop = 0, rightTop = 0;
-    var leftLeft = 0, rightLeft = horizontalSpan(es[0]);
+function px(x) {
+    return "" + x + "px";
+}
+
+function absolutePosition(es) {
+    var i;
 
     makeParentPositionRelative(es[0]);
 
     for (i = 0; i < es.length; i++) {
-        e = es[i];
-        e.style.position = "absolute";
-        if (leftTop <= rightTop) {
-            e.style.top = "" + leftTop + "px";
-            e.style.left = "" + leftLeft + "px";
-            leftTop += verticalSpan(e);
-        } else {
-            e.style.top = "" + rightTop + "px";
-            e.style.left = "" + rightLeft + "px";
-            rightTop += verticalSpan(e);
-        }
+        es[i].style.position = "absolute";
     }
 }
 
-window.addEventListener("load", function (event) {
-    var entries = document.getElementsByClassName("entry");
+function leftMostMin(xs) {
+    var tmp = [xs[0], 0];
+    var i;
 
-    if (document.body.offsetWidth >= 2*horizontalSpan(entries[0]))
-        position_in_two_cols(entries);
+    for (i = 1; i < xs.length; i++) {
+        if (xs[i] < tmp[0]) {
+            tmp = [xs[i], i];
+        }
+    }
+    return tmp;
+}
+
+function numOfCols(e, containerWidth) {
+    return Math.floor(containerWidth / horizontalSpan(e));
+}
+
+// position entries es in n columns
+function dynamicPositioning(es, n) {
+    console.log("dynamicPositioning ", n);
+    var i, e;
+    var tops = [];
+    var hspan = horizontalSpan(es[0]);
+    var top, idx;
+
+    tops.length = n;
+    for (i = 0; i < tops.length; i++) {
+        tops[i] = 0;
+    }
+
+    for (i = 0; i < es.length; i++) {
+        e = es[i];
+        [top, idx] = leftMostMin(tops);
+        e.style.top = px(top);
+        e.style.left = px(hspan * idx);
+        tops[idx] += verticalSpan(e);
+    }
+}
+
+window.addEventListener("load", function () {
+    var entries = document.getElementsByClassName("entry");
+    var ncols;
+
+    absolutePosition(entries);
+    ncols = numOfCols(entries[0], document.body.offsetWidth);
+    dynamicPositioning(entries, ncols);
+
+    window.onresize = function () {
+        var n = numOfCols(entries[0], document.body.offsetWidth);
+        if (n !== ncols) {
+            dynamicPositioning(entries, n);
+            ncols = n;
+        }
+    };
 });
